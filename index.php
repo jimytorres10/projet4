b30b5da8d478d7cbc6a52e129b562b6cb0d90704
jimytorres10 <44463193+jimytorres10@users.noreply.github.com>
2018-10-31 10:05:25 +0100

    ajout routeur pour page un article

--- index.php
+++ index.php
@@ -10,7 +10,16 @@ if (isset($_GET['action']))
         $blogArticles = new Controller();
         $blogArticles->listpost();
     }
-   
+    elseif ($_GET['action'] == 'post') {
+        if (isset($_GET['id']) && $_GET['id'] > 0) {
+            $blogArticles = new Controller();
+            $blogArticles->post($_GET['id']);
+            
+        }
+        else {
+            echo 'Erreur : aucun identifiant de billet envoyé';
+        }
+    }
 }
 else{
     $blogArticles = new Controller();
