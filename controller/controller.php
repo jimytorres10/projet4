2dcbf95ab23085bde4660582aa2fb12a6d399ff8
jimytorres10 <44463193+jimytorres10@users.noreply.github.com>
2018-10-31 10:02:28 +0100

    ajout function permettant d'avoir un seul article

--- controller/controller.php
+++ controller/controller.php
@@ -10,5 +10,11 @@ class Controller
         $req = $blogManager->getList();
         require ('view/mainPagePostView.php');
     }
-
+     public function post($id)
+    {
+        $db = new PDO('mysql:host=localhost;dbname=blogjf;charset=utf8', 'root', 'root');
+        $blogManager = new BlogManager($db);
+        $article = $blogManager->get($id);
+        require ('view/postView.php');
+    }
 }
