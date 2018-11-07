bb6bc5bd8c2a54d7435273adbdd5005b45597d99
jimytorres10 <44463193+jimytorres10@users.noreply.github.com>
2018-11-07 16:33:14 +0100

    ajout de classs bootstrap + changement nom variable

--- view/mainPagePostView.php
+++ view/mainPagePostView.php
@@ -6,8 +6,8 @@ foreach ($req as $value)
 	?>
 	 <div class="row">
                     
-     <div class="article">
-         <h2><?php echo $value->title(); ?> par <?php echo $value->author();?></h2>
+     <div class="article col-md-12">
+         <h2><a href='index.php?action=post&amp;id=<?php echo $value->id();?>'><?php echo $value->title(); ?> par <?php echo $value->author();?></a></h2>
          <p><em>publié le 24/05/98 à 18:50:50</em></p>
          <p><?php echo $value->content();?></p>
      <hr class="sepa">
@@ -15,6 +15,6 @@ foreach ($req as $value)
      </div>
      <?php 
 }
-$coucou = ob_get_clean();
+$content = ob_get_clean();
 require ('view/template.php');
 
