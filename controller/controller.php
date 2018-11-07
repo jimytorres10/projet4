cfb84d8ce044747edf88b6b97722ffa767581b94
jimytorres10 <44463193+jimytorres10@users.noreply.github.com>
2018-11-07 16:37:18 +0100

    ajout controlleur

--- controller/controller.php
+++ controller/controller.php
@@ -10,11 +10,36 @@ class Controller
         $req = $blogManager->getList();
         require ('view/mainPagePostView.php');
     }
-     public function post($id)
+    
+    public function post($id)
     {
         $db = new PDO('mysql:host=localhost;dbname=blogjf;charset=utf8', 'root', 'root');
         $blogManager = new BlogManager($db);
         $article = $blogManager->get($id);
+        $commentManager = new CommentManager($db);
+        $comment = $commentManager->getListByPostId($id);
         require ('view/postView.php');
+    }
+    
+    public function addCom()
+    {
+        
+        
+        $com = new Comment();
+        
+        $com->setAuthor($_POST['pseudo']);
+        $com->setPostId($_POST['id']);
+        $com->setContentCom($_POST['message']);
+        $com->setDatePostCom(date('Y-m-d H:i:s'));
+        $com->setReport(0);
+        
+        $db = new PDO('mysql:host=localhost;dbname=blogjf;charset=utf8', 'root', 'root');
+        $commentManager = new CommentManager($db);
+        $commentManager->add($com);
+        
+        $this->post($_POST['id']);
+        
+        
+        
     }
 }
