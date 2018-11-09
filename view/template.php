42a2ad896a946a7748a7c8af31f79e0def7cb652
jimytorres10 <44463193+jimytorres10@users.noreply.github.com>
2018-11-09 17:06:24 +0100

    class col-md sur menu pour amelioration du responsive

--- view/template.php
+++ view/template.php
@@ -4,7 +4,7 @@
 <head>
 <meta charset="utf-8" />
 <title>Titre</title>
-<link rel="stylesheet" href="style.css" />
+<link rel="stylesheet" href="utilitaire/style.css" />
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
 </head>
@@ -12,30 +12,30 @@
 <body>
 
 <div class="container flex">
-<div>
-<h1>JEAN FORTEROCHE</h1>
-</div>
-<div class="main_menu">
-<ul>
-<li><a><i class="fas fa-home"></i> Accueil</a></li>
-<li><a>Chapitre</a></li>
-<li><a>Contact</a></li>
-</ul>
-</div>
-
+	<div class='row'>
+		<div class="md-col-6 col-sm-12">
+		<h1>JEAN FORTEROCHE</h1>
+		</div>
+        <div class="main_menu md-col-6 col-sm-12">
+        <ul>
+        <li><a href='index.php'><i class="fas fa-home"></i> Accueil</a></li>
+        
+		</ul>
+		</div>
+	</div>
 </div>
 
 <header>
-<div class="container-fluid image">
+<div class="container image">
 
 <h1 class="titre">Billet simple pour l'Alaska</h1>
-    
+   </header> 
         </div>
         <section>
             <div class="container">
-                <?php echo $coucou;?>
+                <?php echo $content;?>
             </div>
         </section>
-    </header>
+    
     </body>
 </html>
