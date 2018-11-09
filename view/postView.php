b926b55ad35a5093e852ec2061f401907cb74675
jimytorres10 <44463193+jimytorres10@users.noreply.github.com>
2018-11-09 17:04:51 +0100

    ajout heure et date commentaire et article + mise en page des commentaires

--- view/postView.php
+++ view/postView.php
@@ -7,18 +7,26 @@ ob_start();
                     
      <div class="article col-md-12">
          <h2><?php echo $article->title(); ?> par <?php echo $article->author();?></h2>
-         <p><em>publié le 24/05/98 à 18:50:50</em></p>
+         <p><em>publié <?php echo $article->jour();?>/<?php echo $article->mois(); ?>/<?php echo $article->annee();?> à <?php echo $article->heure();?>:<?php echo $article->minute();?></em></p>
          <p><?php echo $article->content();?></p>
      <hr class="sepa">
       </div>
      </div>
      <h2>Commentaires</h2>
+     
      <?php 
      foreach ($comment as $value)
      {
-         ?> <p><?php echo $value->contentCom();?></p><?php 
+         ?> 
+         <div class="com col-md-12">
+             <p>Commentaire de <strong><?php echo $value->author();?></strong> du <?php echo $value->jour();?>/<?php echo $value->mois(); ?>/<?php echo $value->annee();?> à <?php echo $value->heure();?>:<?php echo $value->minute();?> :</p>
+             <p class="comText">" <?php echo $value->contentCom();?> "</p>
+             <a href='index.php?action=reportCom&amp;id=<?php echo $value->id();?>'> Signaler ce commentaire</a>
+         </div>
+         <?php 
      }
      ?>
+     
      <h2>Ajouter un commentaire</h2>
      <div class="row">
       	<div class="col-md-12">
