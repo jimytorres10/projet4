0ba7bda50be6e1d34e1dee292dedf86c05db4634
jimytorres10 <44463193+jimytorres10@users.noreply.github.com>
2018-11-09 17:03:28 +0100

    ajout de l'heure et date de publication article

--- view/mainPagePostView.php
+++ view/mainPagePostView.php
@@ -8,7 +8,7 @@ foreach ($req as $value)
                     
      <div class="article col-md-12">
          <h2><a href='index.php?action=post&amp;id=<?php echo $value->id();?>'><?php echo $value->title(); ?> par <?php echo $value->author();?></a></h2>
-         <p><em>publié le 24/05/98 à 18:50:50</em></p>
+         <p><em>publié le <?php echo $value->jour();?>/<?php echo $value->mois(); ?>/<?php echo $value->annee();?> à <?php echo $value->heure();?>:<?php echo $value->minute();?></em></p>
          <p><?php echo $value->content();?></p>
      <hr class="sepa">
       </div>
