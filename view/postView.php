2887f47024a5333fa957fc20891a76b2bc063640
jimytorres10 <44463193+jimytorres10@users.noreply.github.com>
2018-11-28 16:22:58 +0100

    changement de style + modification affichage heure
    + ajout required sur input des commentaires

--- view/postView.php
+++ view/postView.php
@@ -1,4 +1,5 @@
 <?php 
+
 ob_start();
   
 
@@ -7,21 +8,30 @@ ob_start();
                     
      <div class="article col-md-12">
          <h2><?php echo $article->title(); ?> par <?php echo $article->author();?></h2>
-         <p><em>publié <?php echo $article->jour();?>/<?php echo $article->mois(); ?>/<?php echo $article->annee();?> à <?php echo $article->heure();?>:<?php echo $article->minute();?></em></p>
+         <p><em>publié le <?php echo $article->jour();?>/<?php echo $article->mois(); ?>/<?php echo $article->annee();?> à <?php echo $article->heure();?>:<?php if ($article->minute() <= 10) {echo '0'.$article->minute();}
+         else{echo $article->minute();}?></em></p>
          <p><?php echo $article->content();?></p>
      <hr class="sepa">
       </div>
      </div>
-     <h2>Commentaires</h2>
+     <h2 id='com'>Commentaires</h2>
      
      <?php 
      foreach ($comment as $value)
      {
          ?> 
          <div class="com col-md-12">
-             <p>Commentaire de <strong><?php echo $value->author();?></strong> du <?php echo $value->jour();?>/<?php echo $value->mois(); ?>/<?php echo $value->annee();?> à <?php echo $value->heure();?>:<?php echo $value->minute();?> :</p>
+             <p>Commentaire de <strong><?php echo $value->author();?></strong> du <?php echo $value->jour();?>/<?php echo $value->mois(); ?>/<?php echo $value->annee();?> à <?php echo $value->heure();?>:<?php if ($value->minute() <= 10) {echo '0'.$value->minute();}
+         else{echo $value->minute();}?></p>
              <p class="comText">" <?php echo $value->contentCom();?> "</p>
+             <?php 
+             if (isset($_SESSION['com'.$value->id()]) && $_SESSION['com'.$value->id()] == 'ok')
+                {
+                   ?> <p>commentaire signalé</p><?php 
+                }
+                else{?>
              <a href='index.php?action=reportCom&amp;id=<?php echo $value->id();?>'> Signaler ce commentaire</a>
+         <?php }?>
          </div>
          <?php 
      }
@@ -30,9 +40,9 @@ ob_start();
      <h2>Ajouter un commentaire</h2>
      <div class="row">
       	<div class="col-md-12">
-          <form class="comment" method='post' action='index.php?action=com'>
-              <label>Votre pseudo :<input type="text" name="pseudo" class="form-control"></label><br>
-              <label>Votre commentaire :<textarea class="form-control" id="exampleFormControlTextarea1" rows="3" cols="150" name='message'></textarea></label><br>
+          <form class="comment" method='post' action='index.php?action=com#com'>
+              <label>Votre pseudo :<input required type="text" name="pseudo" class="form-control"></label><br>
+              <label>Votre commentaire :<textarea required class="form-control" id="exampleFormControlTextarea1" rows="3" cols="150" name='message'></textarea></label><br>
               <input type="submit" name="" class="btn btn-primary" ><br>
               <input type='hidden' name='id' value='<?php echo $article->id();?>'>
           </form>
@@ -40,5 +50,5 @@ ob_start();
      </div>
 	<?php 
 $content = ob_get_clean();
-
+$title = 'Jean Forteroche -'.$article->title();
 require ('view/template.php');
